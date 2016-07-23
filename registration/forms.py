639bc13615d863c42361eb9422ceefdb00ab2724
yingle <linyingle@gmail.com>
2016-07-23 16:54:07 +0200

    finished module registration

--- registration/forms.py
+++ registration/forms.py
@@ -1,10 +1,33 @@
 from django import forms
-from django.contrib import admin
+from django.contrib.auth.models import User
 
+class CreateUserForm(forms.Form):
+    required_css_class = 'required'
+    username = forms.RegexField(regex=r'^[\w.@+-]+$',
+                                max_length=30,
+                                label="Username",
+                                error_messages={'invalid': "This value may contain only letters, numbers and @/./+/-/_ characters."})
+    email = forms.EmailField(label="E-mail")
+    password1 = forms.CharField(widget=forms.PasswordInput,
+                                label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput,
+                                label="Password (again)")
 
-class RegistrationForm(forms.Form):
-    Username = forms.CharField(max_length=30)
-    FirstName = forms.CharField(max_length=30)
-    LastName = forms.CharField(max_length=30)
-    Email = forms.EmailField()
-    Password = forms.CharField(max_length=30)
+    def clean_username(self):
+        existing = User.objects.filter(username__iexact=self.cleaned_data['username'])
+        if existing.exists():
+            raise forms.ValidationError("A user with that username already exists.")
+        else:
+            return self.cleaned_data['username']
+
+    def clean_email(self):
+        #if you want unique email address. else delete this function
+        if User.objects.filter(email__iexact=self.cleaned_data['email']):
+            raise forms.ValidationError("This email address is already in use. Please supply a different email address.")
+        return self.cleaned_data['email']
+
+    def clean(self):
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError("The two password fields didn't match.")
+        return self.cleaned_data
--- registration/views.py
+++ registration/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.contrib.auth.models import Permission
 from forms import *
 
 # Create your views here.
 def registration_view(request):
     #go to the registration page
     if request.method == 'POST':
-        return HttpResponse("You are at the winningest photo.")
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            new_user = User.objects.create_user(username, email, password)
+            new_user.is_active = True
+            #give permession to add photos
+            permission = Permission.objects.get(name='Can add photo')
+            new_user.user_permissions.add(permission)
+            new_user.save()
+            return render_to_response('registration/success.html')
+        else:
+            return render_to_response('registration/unsuccess.html')
     else:
-        form = RegistrationForm()
+        form = CreateUserForm()
         return render(request, 'registration/registration.html', {'form':form,})
